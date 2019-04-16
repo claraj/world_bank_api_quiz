a0dbb778c1245ea3e16be9a4a1bc47437d2ed48f
clara <10088152+claraj@users.noreply.github.com>
2019-04-16 11:27:30 -0500

    tidy instructions

--- game.js
+++ game.js
@@ -3,24 +3,28 @@ let userAnswerElement = document.querySelector("#user-answer")
 let submitButton = document.querySelector("#submit-answer")
 let resultTextElement = document.querySelector('#result')
 
-
-// TODO when the page loads, select an element at random from the countriesAndCodes array
-// This array is defined in the countries.js file. Your browser treats all 
-// JavaScript files as one big file, organized in the order of the script tags
-// so countriesAndCodes is available to this file 
+// TODO finish the script to challenge the user about their knowledge of capital cities.
+// An array of country codes is provided in the countries.js file. 
+// Your browser treats all JavaScript files as one big file, o
+// organized in the order of the script tags so the countriesAndCodes array is available to this script.
 
 console.log(countriesAndCodes)  // You don't need to log countriesAndCodes - just proving it is available 
+
+
+// TODO when the page loads, select an element at random from the countriesAndCodes array
 
 // TODO display the country's name in the randomCountryElement 
 
 // TODO add a click event handler to the submitButton.  When the user clicks the button,
 //  * read the text from the userAnswerElement 
 //  * Use fetch() to make a call to the World Bank API with the country code (from countriesAndCodes)
-//  * Extract the capital city from the World Bank API response
+//  * Verify no errors were encountered in the API call. If an error occurs, display an alert message. 
+//  * If the API call was successful, extract the capital city from the World Bank API response.
 //  * Compare it to the user's answer. 
-//      You can decide how correct you require the user to be. A basic solution requires 
-//      the user's answer to be exactly the same as the World Bank answer. If you want 
-//      to be more flexible, include and use a string similarity library such as https://github.com/hiddentao/fast-levenshtein 
-//  * Display an appropriate result in the resultTextElement. 
+//      You can decide how correct you require the user to be. At the minimum, the user's answer should be the same
+//      as the World Bank data - make the comparison case insensitive.
+//      If you want to be more flexible, include and use a string similarity library such as https://github.com/hiddentao/fast-levenshtein 
+//  * Finally, display an appropriate message in the resultTextElement to tell the user if they are right or wrong. 
 //      For example "Correct! The capital of Germany is Berlin" or "Wrong - the capital of Germany is not G, it is Berlin"
+
 
